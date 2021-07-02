0a71266e81b49bcbb4ad9f1adc56c9506f4e5926
lenovo <yosepalexsander9@gmail.com>
2021-07-02 19:44:38 +0700

    done useHistory

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 
+import Header from "./components/Header";
+
 function App() {
   // code inside div
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/profile">Profile</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      <Header />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
