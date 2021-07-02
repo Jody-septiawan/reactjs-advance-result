e27eeabd69442be23486671f7fd5727a3bbc8895
lenovo <yosepalexsander9@gmail.com>
2021-07-02 23:42:00 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
-
-import Header from "./components/Header";
 import DetailProduct from "./pages/DetailProduct";
 import PrivateRoute from "./components/PrivateRoute";
 function App() {
   // code inside div
   return (
     <Router>
-      <Header />
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
--- src/pages/DetailProduct.js
+++ src/pages/DetailProduct.js
@@ -1,11 +1,25 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 const DetailProduct = () => {
+  const [data, setData] = useState(null);
   const params = useParams();
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`)
+      .then((response) => response.json())
+      .then((json) => setData(json));
+    return () => {
+      setData(null);
+    };
+  }, []);
   return (
-    <div>
-      <h1>Detail product page with parameter: {params.id}</h1>
-    </div>
+    <Container>
+      <h1>Data with parameter {params.id} is</h1>
+      <p className="h2">{data?.title}</p>
+      <p>{data?.body}</p>
+    </Container>
   );
 };
 
