32ceeeab532a5f82f31816be58487b9bfe3c5b11
lenovo <yosepalexsander9@gmail.com>
2021-07-02 16:48:40 +0700

    Done inline styling

---
+++ src/components/InlineStyle.js
@@ -0,0 +1,28 @@
+//code here
+const style = {
+  textCenter: {
+    textAlign: "center",
+  },
+  textBigBlue: {
+    fontSize: "48px",
+    color: "blue",
+    fontWeight: "bold",
+  },
+  textMediumOrange: {
+    fontSize: "24px",
+    color: "orange",
+    fontWeight: "bold",
+  },
+};
+
+function InlineStyling() {
+  return (
+    <div style={style.textCenter}>
+      <p style={{ color: "chocolate" }}>This is Chocolate</p>
+      <p style={style.textBigBlue}>This is Blue</p>
+      <p style={style.textMediumOrange}>This is Orange</p>
+    </div>
+  );
+}
+
+export default InlineStyling;
