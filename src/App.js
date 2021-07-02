d9ecea4748ab692c8a70bcd3d52596fb371a1c81
lenovo <yosepalexsander9@gmail.com>
2021-07-02 18:47:06 +0700

    basic routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import ReactBootstrapExample from "./components/ReactBootstrapExample";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
 
 function App() {
   // code inside div
   return (
-    <div>
-      <ReactBootstrapExample />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/profile" component={Profile} />
+        <Route exact path="/signup" component={SignIn} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import { Container, Button } from "react-bootstrap";
+
+function About() {
+  return (
+    <Container className="text-center">
+      <p className="h1">About Us</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <Button>Touch me!</Button>
+    </Container>
+  );
+}
+
+export default About;
