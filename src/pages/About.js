644b87c38de85b65f86225654a51be813d6a3ed8
lenovo <yosepalexsander9@gmail.com>
2021-07-02 22:00:24 +0700

    done prop types checking

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,17 @@
 import { Container, Button } from "react-bootstrap";
-
+import DetailProfile from "../components/DetailProfile";
 function About() {
   return (
     <Container className="text-center">
       <p className="h1">About Us</p>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
       <Button>Touch me!</Button>
+      <DetailProfile
+        title="Dumbways"
+        description="lorem ipsum dolor sit amet lorem lorem lorem"
+        summary="Just our company about"
+        total={125}
+      />
     </Container>
   );
 }
