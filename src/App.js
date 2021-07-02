9117e703a41bdc3757b88a888a6d03664dff7ea9
lenovo <yosepalexsander9@gmail.com>
2021-07-02 22:58:00 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -5,18 +5,30 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 
-import Header from "./components/Header";
-
 function App() {
   // code inside div
   return (
     <Router>
-      <Header />
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
         <Route exact path="/profile" component={Profile} />
-        <Route exact path="/signup" component={SignIn} />
+        <Route exact path="/signin" component={SignIn} />
       </Switch>
     </Router>
   );
